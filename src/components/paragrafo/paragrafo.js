f02357fb5e545eb82fd7958d1e7a9c768eec3c2a
Glauco Morera <gmorerafortes@gmail.com>
2023-05-21 20:51:55 -0300

    Quest de React básico do curso DevQuest

---
+++ src/components/paragrafo/paragrafo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./paragrafo.css";
+
+const Paragrafo = () => {
+  return (
+    <p className="texto">
+      freestar Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+      ultricies felis sodales, rutrum dolor a, laoreet massa. Nunc elementum in
+      est non fringilla. Praesent egestas vulputate quam nec luctus. Maecenas
+      interdum elit ac neque luctus, a sodales metus ornare. Mauris porta
+      scelerisque nibh id sollicitudin. Sed sodales elit et pellentesque
+      lobortis. Nulla eget dui mauris.
+    </p>
+  );
+};
+
+export default Paragrafo
