9eca978725d7922d4b641acdfc14a6fc77a931b3
Glauco Morera <gmorerafortes@gmail.com>
2023-05-22 20:34:11 -0300

    Alteração nas margens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import Paragrafo from './components/paragrafo/paragrafo'
-import Button from './components/button/button';
+import "./App.css";
+import Paragrafo from "./components/paragrafo/paragrafo";
+import Button from "./components/button/button";
 
 function App() {
   return (
     <>
-      <Paragrafo />
+      <div>
+        <Paragrafo />
 
-      <Button label='Clique aqui para baicxar CV' />
+        <Button label="Clique aqui para baicxar CV" />
+      </div>
     </>
   );
 }
